c2eff6d2e4419d76e11118af3b942b20023f6869
cpheighter <cheighterjr@gmail.com>
2022-07-08 12:53:19 -0400

    init

---
+++ ForLoops/Program.cs
@@ -0,0 +1,89 @@
+﻿#region Same For Loop Written 15 Times
+/*
+//1
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//2
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//3
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//4
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//5
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//6
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//7
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}//8
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//9
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//10
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//11
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//12
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//13
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//14
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//15
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+*/
+#endregion
+#region Single For Loop Prints Loop 15 Times
+int x = 0;
+while (x < 15)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+    x++;
+}
+#endregion
